9d9a33845fae1c148e08f6f275cbf8f27027935d
lilchurro <lilchurro@users.noreply.github.com>
2019-03-18 14:02:09 -0400

    Test for saving and loading models

--- tests/test_utils_model.py
+++ tests/test_utils_model.py
@@ -5,9 +5,9 @@ import numpy as np
 
 from utils.Model import Model
 
+model = Model(10, labels=['Unknown'])
 
 def test_augment_data():
-    model = Model(10, labels=['Unknown'])
     a = [[1, 2, 3], [4, 5, 6]]
     x = np.array(a)
     a = ['label1', 'label2', 'label3']
@@ -16,7 +16,6 @@ def test_augment_data():
 
 
 def test_get_features():
-    model = Model(10, labels=['Unknown'])
     with open('tests/test.pcap', 'a'):
         os.utime('tests/test.pcap', None)
     if not os.path.isfile('opts/config.json'):
@@ -24,3 +23,13 @@ def test_get_features():
         copyfile('DeviceClassifier/OneLayer/opts/config.json',
                  'opts/config.json')
     model.get_features('tests/test.pcap')
+
+
+def test_save_and_load():
+    load_path = 'DeviceClassifier/OneLayer/models/OneLayerModel.pkl'
+    save_path = 'tests/test_model'
+    model2 = Model(duration=300)
+    model2.load(load_path, jsn=False)
+    model2.save(save_path, jsn=True)
+    os.path.isfile(save_path)
+    model2.load(save_path, jsn=True) is not None
